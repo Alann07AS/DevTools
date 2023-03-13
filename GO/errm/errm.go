5d2b542adcac9210c7e2a0767637bc9270311559
Alann <SchnebelenAlann@hotmail.com>
2023-03-13 08:22:29 +0100

    add: getFilleAndLine private func, FatalErr, FuncExecIfErr

--- GO/errm/errm.go
+++ GO/errm/errm.go
@@ -5,9 +5,27 @@ import (
 	"runtime"
 )
 
+func getFileAndLine() (string, int) {
+	_, file, line, _ := runtime.Caller(2)
+	return file, line
+}
+
 func LogErr(err error) {
 	if err != nil {
-		_, file, line, _ := runtime.Caller(1)
+		file, line := getFileAndLine()
 		log.Printf("[%s:%d] ERROR: %s\n", file, line, err)
+	}
+}
+
+func FatalErr(err error) {
+	if err != nil {
+		file, line := getFileAndLine()
+		log.Fatalf("[%s:%d] FATAL ERROR: %s\n", file, line, err)
+	}
+}
+
+func FuncExecIfErr(err error, f func()) {
+	if err != nil {
+		f()
 	}
 }
